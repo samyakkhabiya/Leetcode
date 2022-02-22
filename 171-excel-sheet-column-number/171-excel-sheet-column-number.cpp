c6fc58791090e4c49b9946a12a6d91bc52edbefd
samyakkhabiya <85106734+samyakkhabiya@users.noreply.github.com>
2022-02-22 18:56:26 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (92.06%) - LeetHub

---
+++ 171-excel-sheet-column-number/171-excel-sheet-column-number.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int titleToNumber(string col) {
+         int result = 0;
+        for(char c : col)
+        {
+			//d = s[i](char) - 'A' + 1 (we used  s[i] -  'A' to convert the letter to a number like it's going to be C)
+
+            int d = c - 'A' + 1;
+            result = result * 26 + d;
+        }
+        return result;
+    }
+};
