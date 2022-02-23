de78e74ea591fe41afd68d45159171ed95e81f96
samyakkhabiya <85106734+samyakkhabiya@users.noreply.github.com>
2022-02-23 18:06:43 +0530

    Time: 158 ms (48.92%), Space: 23.3 MB (34.36%) - LeetHub

---
+++ 2125-number-of-laser-beams-in-a-bank/2125-number-of-laser-beams-in-a-bank.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int numberOfBeams(vector<string>& bank) {
+        
+        vector<int> num;  // to store the camera cnt at each lvl if cnt>0
+        int cnt=0;
+			for(int i=0;i<bank.size();i++){  // counting camera at all lvl
+            cnt=0;
+            for(int j=0;j<bank[0].size();j++){
+                if(bank[i][j]=='1'){
+                    cnt++;
+                }                
+            }
+            if(cnt>0)  //only need to push if cnt>0
+                num.push_back(cnt);
+        }
+        
+        if(num.size()<=1){  //corner case
+            return 0;
+        }
+		
+        long long sum=0 ;
+        for(int i=0;i<num.size()-1;i++){   // calc final ans
+            sum+=(num[i]*num[i+1]);
+        }
+        
+        return sum;
+        
+    }
+};
