362b3e00c9addde69168b7c1b03a6e82096b2bfd
samyakkhabiya <85106734+samyakkhabiya@users.noreply.github.com>
2022-03-30 20:39:43 +0530

    Time: 778 ms (41.63%), Space: 137.8 MB (89.29%) - LeetHub

---
+++ 2001-number-of-pairs-of-interchangeable-rectangles/2001-number-of-pairs-of-interchangeable-rectangles.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    long long interchangeableRectangles(vector<vector<int>>& rectangles) 
+{
+	long long result = 0;
+	map<pair<int, int>, int> mp;
+
+	for (auto& rect : rectangles)
+	{
+		int gcd = __gcd(rect[0], rect[1]);
+		pair<int, int> key = {rect[0]/gcd, rect[1]/gcd};
+		if(mp.find(key) != mp.end()) result += mp[key];
+		mp[key]++;
+	}
+
+	return result;
+}
+};
