674779ec8a10508abd55703f490bd1137ef1df76
samyakkhabiya <85106734+samyakkhabiya@users.noreply.github.com>
2022-04-09 01:48:40 +0530

    Added solution - LeetHub

--- Edit Distance - GFG/edit-distance.cpp
+++ Edit Distance - GFG/edit-distance.cpp
@@ -6,32 +6,33 @@ using namespace std;
 class Solution {
   public:
   
-    int dpmem(string& w1, string& w2,int i,int j,vector<vector<int>>& dp){
-        if(w1.size()==i && w2.size()==j)
+    int dfs(string &word1, string &word2, int i, int j, vector<vector<int>> &dp){
+        if(i==word1.size() && j==word2.size())
             return 0;
+        if(word1.size()==i)
+            return word2.size()-j;
+        if(word2.size()==j)
+            return word1.size()-i;
         
-        if(w1.size()==i)
-            return w2.size()-j;
-        if(w2.size()==j)
-            return w1.size()-i;
         if(dp[i][j]!=-1)
             return dp[i][j];
-        
-        if(w1[i]==w2[j])
-            return dp[i][j]=dpmem(w1,w2,i+1,j+1,dp);
-        else{
-            int ins=1+dpmem(w1,w2,i+1,j,dp);
-            int rem=1+dpmem(w1,w2,i,j+1,dp);
-            int del=1+dpmem(w1,w2,i+1,j+1,dp);
-            dp[i][j]=min(ins,min(rem,del));
+            
+        if(word1[i]==word2[j]){
+            return dp[i][j]=dfs(word1,word2,i+1,j+1,dp);
         }
-        return dp[i][j];
+        else{
+            int a=dfs(word1,word2,i+1,j,dp)+1;
+            int b=dfs(word1,word2,i,j+1,dp)+1;
+            int c=dfs(word1,word2,i+1,j+1,dp)+1;
+            return dp[i][j]=min({a,b,c});
+        }
+        
     }
     
     int editDistance(string word1, string word2) {
         // Code here
-        vector<vector<int>> dp(word1.size()+1,vector<int>(word2.size(),-1));
-        return dpmem(word1,word2,0,0,dp);
+        vector<vector<int>> dp(word1.size()+1,vector<int>(word2.size()+1,-1));
+        return dfs(word1,word2,0,0,dp);
         
     }
 };
