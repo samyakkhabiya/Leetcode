200616418d2c55e21250093116102676cd5c34cd
samyakkhabiya <85106734+samyakkhabiya@users.noreply.github.com>
2022-06-24 15:42:15 +0530

    Time: 2755 ms (5.04%), Space: 272.2 MB (5.68%) - LeetHub

---
+++ 630-course-schedule-iii/630-course-schedule-iii.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    static bool cmp(vector<int> a, vector<int> b){
+        return a[1]<b[1];
+    }
+    int scheduleCourse(vector<vector<int>>& courses) {
+        sort(courses.begin(),courses.end(),cmp);
+        int sum=0;
+        priority_queue<int> pq;
+        for(auto i:courses){
+            sum+=i[0];
+            pq.push(i[0]);
+            if(sum>i[1]){
+                sum-=pq.top();
+                pq.pop();
+            }
+        }
+        return pq.size();
+    }
+};
