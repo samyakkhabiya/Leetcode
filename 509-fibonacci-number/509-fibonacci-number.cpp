b8714263fdf94fc07ade1fbcb8ea1b03bd0cd66a
samyakkhabiya <85106734+samyakkhabiya@users.noreply.github.com>
2022-07-07 00:36:50 +0530

    Time: 37 ms (5.64%), Space: 5.8 MB (80.05%) - LeetHub

---
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int dp[31];
+    int solve(int n){
+        if(n<=1)
+            return n;
+        if(dp[n]!=-1)
+            return dp[n];
+        return solve(n-1)+solve(n-2);
+    }
+    
+    int fib(int n) {
+        memset(dp,-1,sizeof(dp));
+        return solve(n);
+    }
+};
